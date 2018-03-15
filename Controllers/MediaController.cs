3e60e2c1dc909f7c8b99c4d14e6cb8bf26982baf
Lawrence Dine <lawrence@moov2.com>
2018-03-15 13:21:56 +0000

    Update MediaController to filter Secure
    Update the MediaController to filter IsSecure media items when the user
    does not have permission to view secure media items.

--- Controllers/MediaController.cs
+++ Controllers/MediaController.cs
@@ -1,6 +1,11 @@
-﻿using Orchard.Environment.Extensions;
+﻿using Moov2.Orchard.Azure.PassThrough.Models;
+using Orchard.ContentManagement;
+using Orchard.Environment.Extensions;
 using Orchard.FileSystems.Media;
+using Orchard.MediaLibrary.Services;
 using Orchard.Security;
+using System.IO;
+using System.Linq;
 using System.Web.Mvc;
 
 namespace Moov2.Orchard.Azure.PassThrough.Controllers
@@ -9,22 +14,30 @@ namespace Moov2.Orchard.Azure.PassThrough.Controllers
     public class MediaController : Controller
     {
         private readonly IAuthorizer _authorizer;
+        private readonly IMediaLibraryService _mediaLibraryService;
         private readonly IStorageProvider _storageProvider;
 
-        public MediaController(IAuthorizer authorizer, IStorageProvider storageProvider)
+        public MediaController(IAuthorizer authorizer, IMediaLibraryService mediaLibraryService, IStorageProvider storageProvider)
         {
             _authorizer = authorizer;
+            _mediaLibraryService = mediaLibraryService;
             _storageProvider = storageProvider;
         }
 
         public ActionResult Index(string mediaPath)
         {
-            if (!_authorizer.Authorize(Permissions.ViewSecureMedia))
-            {
-                return HttpNotFound();
-            }
             mediaPath = _storageProvider.GetStoragePath(mediaPath);
-            if (!_storageProvider.FileExists(mediaPath))
+            if (mediaPath == null)
+                return HttpNotFound();
+            var fileName = Path.GetFileName(mediaPath);
+            var directory = mediaPath.Substring(0, mediaPath.IndexOf(fileName));
+            directory = directory.TrimEnd('/');
+            var medias = _mediaLibraryService
+                .GetMediaContentItems(VersionOptions.Published)
+                .Where(x => x.FolderPath == directory && x.FileName == fileName)
+                .List();
+            var isSecure = medias.All(x => x.As<SecureMediaPart>()?.IsSecure ?? true);
+            if ((isSecure && !_authorizer.Authorize(Permissions.ViewSecureMedia)) || !_storageProvider.FileExists(mediaPath))
             {
                 return HttpNotFound();
             }
